3e53e80481cefed52956e3a394830f2dfd6dfdee
MUmarJ <umarjh96@gmail.com>
2023-08-06 19:21:04 -0500

    Add Farms Page

---
+++ packages/nextjs/components/FarmCard.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+export default function FarmCard({ farm }: any) {
+  const { description, img, title } = farm;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia component="img" height="140" image={img} alt="Image Not Found" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title || "No Title"}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description || "No Description"}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -57,6 +57,12 @@ export const Header = () => {
           Block Explorer
         </NavLink>
       </li>
+      <li>
+        <NavLink href="/farms">
+          <MagnifyingGlassIcon className="h-4 w-4" />
+          Farms
+        </NavLink>
+      </li>
     </>
   );
 
@@ -86,7 +92,7 @@ export const Header = () => {
           )}
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6">
-          <div className="flex relative" style={{height: "2.5rem", width: "4rem"}}>
+          <div className="flex relative" style={{ height: "2.5rem", width: "4rem" }}>
             <Image alt="FarmShare logo" className="cursor-pointer" fill src="/fs-logo-wide.png" />
           </div>
           <div className="flex flex-col">
