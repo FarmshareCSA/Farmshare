1851b46418b3e8492848f08a80efd7a0a492980b
MUmarJ <umarjh96@gmail.com>
2023-08-16 19:40:51 -0400

    Modify AddressValidationForm to hide API KEY

--- packages/nextjs/components/AddressValidationForm.tsx
+++ packages/nextjs/components/AddressValidationForm.tsx
@@ -2,21 +2,10 @@
 import React, { useState } from "react";
 import { InputBase } from "./scaffold-eth";
 import { Client } from "@googlemaps/google-maps-services-js";
-import { z } from "zod";
 
-// const { REACT_APP_GMAPS_API_KEY } = process.env;
-const REACT_APP_GMAPS_API_KEY = "AIzaSyA_Hgokq2qwvhF8chsX3JHwd9IJbg68qMU";
-console.log(REACT_APP_GMAPS_API_KEY);
-
-const AddressValidationSchema = z.object({
-  street: z.string().nonempty("Street is required"),
-  city: z.string().nonempty("City is required"),
-  state: z.string().nonempty("State is required"),
-  zip: z
-    .string()
-    .nonempty("Zip code is required")
-    .regex(/^\d{5}(-\d{4})?$/, "Zip code is invalid"),
-});
+const { REACT_APP_GMAPS_API_KEY } = process.env;
+// const REACT_APP_GMAPS_API_KEY = "AIzaSyA_Hgokq2qwvhF8chsX3JHwd9IJbg68qMU";
+// console.log(REACT_APP_GMAPS_API_KEY);
 
 const AddressValidationForm = () => {
   const [location, setLocation] = useState("");
@@ -52,7 +41,7 @@ const AddressValidationForm = () => {
         value={location}
         onChange={e => setLocation(e)}
         // placeholder="123 Main St., Farmtown, NY"
-        placeholder={REACT_APP_GMAPS_API_KEY || "NO KEY"}
+        placeholder={"123 Main St., Farmtown, NY" || "NO KEY"}
         prefix={<span className="self-center cursor-pointer text-xl font-semibold px-4 text-accent">📍</span>}
       />
       <button onClick={validateAddress}>Validate Address</button>
