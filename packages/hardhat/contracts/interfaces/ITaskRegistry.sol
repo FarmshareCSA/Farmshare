fe742672c8af68c19d7d205a24584db3189df747
webthethird <ratzbodell@gmail.com>
2023-08-18 16:05:56 -0400

    Add events

--- packages/hardhat/contracts/interfaces/ITaskRegistry.sol
+++ packages/hardhat/contracts/interfaces/ITaskRegistry.sol
@@ -8,24 +8,47 @@ import "../FarmShareTokens.sol";
 interface ITaskRegistry {
 
     event TaskRegistered(
-      bytes32 indexed taskUID, 
-      bytes32 indexed communityUID,
-      string name, 
-      string description, 
-      address creator,
-      uint256 startTime,
-      uint256 endTime,
-      bool recurring,
-      uint256 frequency
+        bytes32 indexed taskUID, 
+        bytes32 indexed communityUID,
+        string name, 
+        string description, 
+        address creator,
+        uint256 startTime,
+        uint256 endTime,
+        bool recurring,
+        uint256 frequency
     );
 
     event TaskFunded(
-      bytes32 indexed taskUID,
-      address tokenAddress,
-      bool isErc1155,
-      bool isErc20,
-      uint256 amount,
-      uint256 tokenId
+        bytes32 indexed taskUID,
+        bytes32 indexed fundingUID,
+        address tokenAddress,
+        bool isErc1155,
+        bool isErc20,
+        uint256 amount,
+        uint256 tokenId
+    );
+
+    event TaskStarted(
+        bytes32 indexed taskUID,
+        bytes32 indexed startedUID,
+        bytes32 indexed userUID,
+        uint256 startTimestamp
+    );
+
+    event TaskCompleted(
+        bytes32 indexed taskUID,
+        bytes32 indexed completedUID,
+        bytes32 indexed userUID,
+        uint256 completedTimestamp
+    );
+
+    event RewardPaid(
+        bytes32 indexed taskUID,
+        bytes32 indexed fundingUID,
+        address tokenAddress,
+        address recipient,
+        uint256 amount
     );
 
     function shareTokens() external view returns (FarmShareTokens);
