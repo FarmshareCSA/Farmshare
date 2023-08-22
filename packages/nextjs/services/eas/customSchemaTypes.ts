43fa7fa4bcdf7f57f232bb2fdba989d7f8aea1e6
webthethird <ratzbodell@gmail.com>
2023-08-22 15:17:24 -0500

    Add Farm, Task and TaskReward types

--- packages/nextjs/services/eas/customSchemaTypes.ts
+++ packages/nextjs/services/eas/customSchemaTypes.ts
@@ -19,6 +19,47 @@ export type Community = {
   imageURL: string;
 };
 
+export type Farm = {
+  uid: string;
+  name: string;
+  owner: string;
+  description: string;
+  streetAddress: string;
+  city: string;
+  state: string;
+  country: string;
+  postalCode: string;
+  latitude: string;
+  longitude: string;
+  websiteURL: string;
+  imageURL: string;
+};
+
+export type Task = {
+  uid: string;
+  communityUID: string;
+  name: string;
+  description: string;
+  creator: string;
+  startTime: number;
+  endTime: number;
+  recurring: boolean;
+  frequency: number;
+  imageURL: string;
+  rewards: TaskReward[];
+};
+
+export type TaskReward = {
+  uid: string;
+  taskUID: string;
+  tokenAddress: string;
+  isErc1155: boolean;
+  isErc20: boolean;
+  amount: number;
+  tokenId: number;
+  tokenName: string;
+};
+
 export type UserRegistration = {
   uid: string;
   account: string;
