db3bbe06bec902e219579c31750b80438cc5faaf
webthethird <ratzbodell@gmail.com>
2023-08-23 01:56:02 -0500

    Get remaining task related attestation data

--- packages/nextjs/pages/tasks.tsx
+++ packages/nextjs/pages/tasks.tsx
@@ -291,6 +291,21 @@ const Tasks: NextPage = () => {
     functionName: "taskFundedSchemaUID",
   });
 
+  const { data: applicationSchemaUID } = useScaffoldContractRead({
+    contractName: "TaskRegistry",
+    functionName: "taskApplicationSchemaUID",
+  });
+
+  const { data: startedSchemaUID } = useScaffoldContractRead({
+    contractName: "TaskRegistry",
+    functionName: "taskStartedSchemaUID",
+  });
+
+  const { data: completedSchemaUID } = useScaffoldContractRead({
+    contractName: "TaskRegistry",
+    functionName: "taskCompletedSchemaUID",
+  });
+
   const rewardSchemaEncoder = new SchemaEncoder(
     "address tokenAddress,bool isErc1155,bool isErc20,uint256 amount,uint256 tokenId,string tokenName",
   );
@@ -298,6 +313,10 @@ const Tasks: NextPage = () => {
   const taskSchemaEncoder = new SchemaEncoder(
     "bytes32 communityUID,string name,string description,address creator,uint256 startTime,uint256 endTime,bool recurring,uint256 frequency,string imageURL",
   );
+
+  const applicationSchemaEncoder = new SchemaEncoder("bytes32 taskUID,bytes32 userUID,bytes32[] skillUIDs");
+  const startedSchemaEncoder = new SchemaEncoder("bytes32 taskUID,bytes32 userUID,uint256 startTimestamp");
+  const completedSchemaEncoder = new SchemaEncoder("bytes32 taskUID,bytes32 userUID,uint256 completeTimestamp");
 
   useEffect(() => {
     // if (communities && communityUID) {
@@ -311,13 +330,19 @@ const Tasks: NextPage = () => {
   }, [communityUID, taskSchemaUID, community, open, userRegistration]);
 
   const getTasks = async () => {
-    if (taskSchemaUID && rewardSchemaUID) {
+    if (taskSchemaUID && rewardSchemaUID && applicationSchemaUID && startedSchemaUID && completedSchemaUID) {
       const taskList = await getTasksForCommunity(
         community,
         taskSchemaUID,
         rewardSchemaUID,
+        applicationSchemaUID,
+        startedSchemaUID,
+        completedSchemaUID,
         taskSchemaEncoder,
         rewardSchemaEncoder,
+        applicationSchemaEncoder,
+        startedSchemaEncoder,
+        completedSchemaEncoder,
       );
       console.log("Found %s tasks for community %s", taskList.length, community);
       setTasks(displayTasks(taskList));
@@ -331,25 +356,22 @@ const Tasks: NextPage = () => {
     setCommunity(value);
   };
 
-
-
   return (
     <React.Fragment>
-      
       <Box style={{ backgroundColor: "#F9FFF1" }}>
-      {communities && (
-        <div className="flex items-left flex-col flex-grow pt-5 pb-5">
-          <div className="px-5">
-            <Select label="Select a Community" value={community} onChange={handleChange}>
-              {communities.map(community => (
-                <MenuItem key={community.uid} value={community.uid}>
-                  {community.name}
-                </MenuItem>
-              ))}
-            </Select>
+        {communities && (
+          <div className="flex items-left flex-col flex-grow pt-5 pb-5">
+            <div className="px-5">
+              <Select label="Select a Community" value={community} onChange={handleChange}>
+                {communities.map(community => (
+                  <MenuItem key={community.uid} value={community.uid}>
+                    {community.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </div>
           </div>
-        </div>
-      )}
+        )}
         <Grid container sx={{ padding: "5px" }}>
           {tasks}
           <Fab
@@ -375,7 +397,7 @@ const Tasks: NextPage = () => {
             <Typography id="modal-modal-title" variant="h6" component="h2">
               Create Task
             </Typography>
-            <TaskCreationForm communityUID={community} onClose={setOpen} getTasks={getTasks}/>
+            <TaskCreationForm communityUID={community} onClose={setOpen} getTasks={getTasks} />
           </Box>
         </Modal>
       </Box>
