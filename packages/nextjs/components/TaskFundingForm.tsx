d1e9a8c9a29f67d9b0dfe37722d10afa30812b89
webthethird <ratzbodell@gmail.com>
2023-08-22 23:07:20 -0500

    Task funding with shares works!

--- packages/nextjs/components/TaskFundingForm.tsx
+++ packages/nextjs/components/TaskFundingForm.tsx
@@ -1,15 +1,16 @@
-import { ChangeEvent, useState } from "react";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import { Spinner } from "./Spinner";
-import { InputBase } from "./scaffold-eth";
-import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
+import { TokenBalance } from "./scaffold-eth/TokenBalance";
 import { FormControlLabel, MenuItem, Select, Switch, TextField } from "@mui/material";
+import { Contract } from "ethers";
 import moment from "moment";
 import invariant from "tiny-invariant";
-import { useNetwork } from "wagmi";
+import { useNetwork, useWalletClient } from "wagmi";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 import { UserRole } from "~~/services/eas/customSchemaTypes";
+import { getUserAttestationsForAddress } from "~~/services/eas/utils";
 import { useGlobalState } from "~~/services/store/store";
+import { walletClientToSigner } from "~~/services/web3/ethers";
 import { getTargetNetwork, notification } from "~~/utils/scaffold-eth";
 import { contracts } from "~~/utils/scaffold-eth/contract";
 
@@ -25,23 +26,53 @@ const MenuProps = {
 };
 
 export const TaskFundingForm = ({ taskUID, onClose }: any) => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [farmUID, setFarmUID] = useState<`0x${string}`>("0x0");
+  const [token, setToken] = useState("0x4200000000000000000000000000000000000006");
   const [startTime, setStartTime] = useState(moment().format("yyyy-MM-DDThh:mm"));
   const [endTime, setEndTime] = useState(moment().add(7, "day").format("yyyy-MM-DDThh:mm"));
   const [submitting, setSubmitting] = useState(false);
   const { chain } = useNetwork();
-  const signer = useGlobalState(state => state.userSigner);
+  let signer = useGlobalState(state => state.userSigner);
+  const account = useGlobalState(state => state.userSmartAccount);
   const userRegistration = useGlobalState(state => state.userRegistration);
   const [fundWithShares, setFundWithShares] = useState(
     userRegistration?.role == UserRole.Farmer || userRegistration?.role == UserRole.Manager,
   );
+  const { data: walletClient } = useWalletClient();
+
+  const taskRegistry =
+    chain && contracts
+      ? contracts[chain.id]?.[0]?.["contracts"]?.["TaskRegistry"]
+        ? contracts[chain.id]?.[0]?.["contracts"]?.["TaskRegistry"].address
+        : "0x39959bfCFBE21E2BE590f4a3EB2Fbe2403Af32cB"
+      : "0x39959bfCFBE21E2BE590f4a3EB2Fbe2403Af32cB";
+
+  const farmSharesAddress =
+    chain && contracts
+      ? contracts[chain.id]?.[0]?.["contracts"]?.["FarmShares"]
+        ? contracts[chain.id]?.[0]?.["contracts"]?.["FarmShares"].address
+        : "0xFF2d8417c275F06e69392C08B6b4292D556409f5"
+      : "0xFF2d8417c275F06e69392C08B6b4292D556409f5";
+
+  const farmSharesABI = [
+    "function mint(address to, uint256 id, uint256 amount, bytes calldata data) public",
+    "function balanceOf(address account, uint256 id) external view returns (uint256 balance)",
+  ];
+
+  const taskRegistryABI = ["function fundTaskWithFarmShares(bytes32 taskUID,bytes32 farmUID,uint amount)"];
+
   const writeDisabled = !chain || chain?.id !== getTargetNetwork().id;
+
+  const { data: farmSchemaUID } = useScaffoldContractRead({
+    contractName: "FarmRegistry",
+    functionName: "registrationSchemaUID",
+  });
 
   const erc20s = [
     {
       name: "USDC",
-      address: "0x853154e2A5604E5C74a2546E2871Ad44932eB92C",
+      address: "0x4e2AA48F22718968D911DD65eDC5a9C59437dF3D",
     },
     {
       name: "WETH",
@@ -52,8 +83,26 @@ export const TaskFundingForm = ({ taskUID, onClose }: any) => {
   const handleSubmit = async () => {
     setSubmitting(true);
     try {
+      if (!signer && walletClient) {
+        signer = walletClientToSigner(walletClient);
+      }
       invariant(signer, "Signer must be defined");
+      if (fundWithShares) {
+        console.log(
+          `Mint args: to: ${taskRegistry} id: ${BigInt(farmUID)} amount: ${BigInt(amount)} taskUID: ${taskUID}`,
+        );
 
+        const taskRegistryContract = new Contract(taskRegistry, taskRegistryABI, signer);
+        const receipt = await taskRegistryContract.fundTaskWithFarmShares(taskUID, farmUID, amount);
+        await receipt.wait();
+        const farmSharesContract = new Contract(farmSharesAddress, farmSharesABI, signer);
+
+        console.log(
+          `TaskRegistry shares balance: ${await farmSharesContract.balanceOf(taskRegistry, BigInt(farmUID))}`,
+        );
+      } else {
+        // Fund with ERC-20
+      }
       setSubmitting(false);
       notification.success("You successfully funded a task");
       onClose(false);
@@ -63,6 +112,20 @@ export const TaskFundingForm = ({ taskUID, onClose }: any) => {
       setSubmitting(false);
     }
   };
+
+  useEffect(() => {
+    const getFarmUID = async () => {
+      if (farmSchemaUID && account) {
+        const farmAttestations = await getUserAttestationsForAddress(account, farmSchemaUID);
+        if (farmAttestations.length > 0) {
+          const farmAttestation = farmAttestations[0];
+          setFarmUID(farmAttestation.id);
+        }
+      }
+    };
+    if (userRegistration && (userRegistration.role == UserRole.Farmer || userRegistration.role == UserRole.Manager))
+      getFarmUID();
+  }, [farmSchemaUID, account, userRegistration]);
 
   return (
     <div className="flex flex-col gap-3 py-5 first:pt-0 last:pb-1">
@@ -79,69 +142,35 @@ export const TaskFundingForm = ({ taskUID, onClose }: any) => {
         />
       )}
       {!fundWithShares && (
-        <Select
-          labelId="select-multiple-chip-label"
-          id="select-multiple-chip"
-          // value={selectedSkills}
-          // onChange={handleChange}
-          MenuProps={MenuProps}
-        >
-          {erc20s.map(erc20 => (
-            <MenuItem key={erc20.address} value={erc20.address}>
-              {erc20.name}
-            </MenuItem>
-          ))}
-        </Select>
+        <>
+          <Select
+            labelId="select-multiple-chip-label"
+            id="select-multiple-chip"
+            value={token}
+            onChange={e => setToken(e.target.value)}
+            MenuProps={MenuProps}
+          >
+            {erc20s.map(erc20 => (
+              <MenuItem key={erc20.address} value={erc20.address}>
+                {erc20.name}
+              </MenuItem>
+            ))}
+          </Select>
+          {account && <TokenBalance address={account} token={token} />}
+        </>
       )}
-      {/* <TextField
-        label="Task Name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        placeholder="Harvest Apples"
+      <TextField
+        label="Amount"
+        value={amount}
+        type="number"
+        onChange={e => setAmount(parseInt(e.target.value))}
+        placeholder="Token amount"
         InputProps={{
-          startAdornment: (
-            <span className="self-center cursor-pointer text-xl font-semibold px-3 text-accent">
-              <Image src="/nametag.png" alt="name tag" width={40} height={40} />
-            </span>
-          ),
+          startAdornment: <span className="self-center cursor-pointer text-xl font-semibold px-3 text-accent">💰</span>,
         }}
       />
-      <TextField
-        label="Task Description"
-        value={description}
-        onChange={e => setDescription(e.target.value)}
-        multiline
-        placeholder="Spend an afternoon picking apples with friends at Apple Pond Farm"
-        InputProps={{
-          startAdornment: <span className="self-center cursor-pointer text-xl font-semibold px-4 text-accent">📝</span>,
-        }}
-      />
-      <TextField
-        id="start-time"
-        label="Start Time"
-        type="datetime-local"
-        value={startTime}
-        defaultValue={startTime}
-        onChange={e => setStartTime(e.target.value)}
-        // className={"flex border-2 border-base-300 bg-base-200 rounded-full text-accent"}
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-      <TextField
-        id="end-time"
-        label="End Time"
-        type="datetime-local"
-        value={endTime}
-        defaultValue={endTime}
-        onChange={e => setEndTime(e.target.value)}
-        // className={"flex border-2 border-base-300 bg-base-200 rounded-full text-accent"}
-        InputLabelProps={{
-          shrink: true,
-        }}
-      /> */}
       <button className={`btn btn-secondary btn-sm`} disabled={writeDisabled || submitting} onClick={handleSubmit}>
-        {submitting ? <Spinner /> : "Create Task 📝"}
+        {submitting ? <Spinner /> : "Fund Task 💸"}
       </button>
     </div>
   );
