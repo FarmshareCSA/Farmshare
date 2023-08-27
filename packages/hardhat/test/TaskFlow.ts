db3e44553cffd2ef115d2d16793a8692f55067a5
webthethird <ratzbodell@gmail.com>
2023-08-27 13:21:25 -0500

    Test user registration

--- packages/hardhat/test/TaskFlow.ts
+++ packages/hardhat/test/TaskFlow.ts
@@ -4,7 +4,7 @@ import { UserRegistry, FarmRegistry, TaskRegistry, FarmShareTokens, CommunityReg
 import { deployProtocol } from "./helpers/DeployProtocol";
 import { Signer } from "ethers";
 import { SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
-import { expectAttestation } from "./helpers/EAS";
+import { expectAttestation, expectFailedAttestation } from "./helpers/EAS";
 
 describe("FarmShare task flow", function () {
   let deployer: Signer;
@@ -61,61 +61,88 @@ describe("FarmShare task flow", function () {
       "address account,string name,bytes32 emailHash,string location,uint8 role",
     );
 
-    before(async () => {
+    beforeEach(async () => {
       userRegistrationSchemaUID = await userRegistryContract.registrationSchemaUID();
-      it("Allows the user and farmer to register", async () => {
-        let encodedData = userSchemaEncoder.encodeData([
-          { name: "account", value: await user.getAddress(), type: "address" },
-          { name: "name", value: "Alice User", type: "string" },
-          {
-            name: "emailHash",
-            value: "0x0102030405060708091011121314151617181920212223242526272829303132",
-            type: "bytes32",
-          },
-          { name: "location", value: "", type: "string" },
-          { name: "role", value: "1", type: "uint8" },
-        ]);
-        let { uid: _userUID } = await expectAttestation(
-          easContract,
-          userRegistrationSchemaUID,
-          {
-            recipient: await user.getAddress(),
-            expirationTime: BigInt(0),
-            data: encodedData,
-          },
-          { from: user },
-        );
-        userUID = _userUID;
-        const userRecord = await userRegistryContract.userRecordByUID(userUID);
-        expect(userRecord.account).to.equal(await user.getAddress());
-        expect(userRecord.name).to.equal("Alice User");
+      let encodedData = userSchemaEncoder.encodeData([
+        { name: "account", value: await user.getAddress(), type: "address" },
+        { name: "name", value: "Alice User", type: "string" },
+        {
+          name: "emailHash",
+          value: "0x0102030405060708091011121314151617181920212223242526272829303132",
+          type: "bytes32",
+        },
+        { name: "location", value: "", type: "string" },
+        { name: "role", value: "1", type: "uint8" },
+      ]);
+      let { uid: _userUID } = await expectAttestation(
+        easContract,
+        userRegistrationSchemaUID,
+        {
+          recipient: await user.getAddress(),
+          expirationTime: BigInt(0),
+          data: encodedData,
+        },
+        { from: user },
+      );
+      userUID = _userUID;
+      const userRecord = await userRegistryContract.userRecordByUID(userUID);
+      expect(userRecord.account).to.equal(await user.getAddress());
+      expect(userRecord.name).to.equal("Alice User");
+    });
 
-        encodedData = userSchemaEncoder.encodeData([
-          { name: "account", value: await farmer.getAddress(), type: "address" },
-          { name: "name", value: "Bob Farmer", type: "string" },
-          {
-            name: "emailHash",
-            value: "0x0001020304050607080910111213141516171819202122232425262728293031",
-            type: "bytes32",
-          },
-          { name: "location", value: "", type: "string" },
-          { name: "role", value: "4", type: "uint8" },
-        ]);
-        let { uid: _farmerUID } = await expectAttestation(
-          easContract,
-          userRegistrationSchemaUID,
-          {
-            recipient: await farmer.getAddress(),
-            expirationTime: BigInt(0),
-            data: encodedData,
-          },
-          { from: farmer },
-        );
-        farmerUID = _farmerUID;
-        const farmerRecord = await userRegistryContract.userRecordByUID(farmerUID);
-        expect(farmerRecord.account).to.equal(await farmer.getAddress());
-        expect(farmerRecord.name).to.equal("Alice User");
-      });
+    it("Does not allow the user to register again with the same address", async () => {
+      userRegistrationSchemaUID = await userRegistryContract.registrationSchemaUID();
+      let encodedData = userSchemaEncoder.encodeData([
+        { name: "account", value: await user.getAddress(), type: "address" },
+        { name: "name", value: "Alice User", type: "string" },
+        {
+          name: "emailHash",
+          value: "0x0102030405060708091011121314151617181920212223242526272829303132",
+          type: "bytes32",
+        },
+        { name: "location", value: "", type: "string" },
+        { name: "role", value: "1", type: "uint8" },
+      ]);
+      await expectFailedAttestation(
+        easContract,
+        userRegistrationSchemaUID,
+        {
+          recipient: await user.getAddress(),
+          expirationTime: BigInt(0),
+          data: encodedData,
+        },
+        { from: user },
+        "User already registered",
+      );
+    });
+
+    it("Allows the farmer to register", async () => {
+      userRegistrationSchemaUID = await userRegistryContract.registrationSchemaUID();
+      let encodedData = userSchemaEncoder.encodeData([
+        { name: "account", value: await farmer.getAddress(), type: "address" },
+        { name: "name", value: "Bob Farmer", type: "string" },
+        {
+          name: "emailHash",
+          value: "0x0001020304050607080910111213141516171819202122232425262728293031",
+          type: "bytes32",
+        },
+        { name: "location", value: "", type: "string" },
+        { name: "role", value: "4", type: "uint8" },
+      ]);
+      let { uid: _farmerUID } = await expectAttestation(
+        easContract,
+        userRegistrationSchemaUID,
+        {
+          recipient: await farmer.getAddress(),
+          expirationTime: BigInt(0),
+          data: encodedData,
+        },
+        { from: farmer },
+      );
+      farmerUID = _farmerUID;
+      const farmerRecord = await userRegistryContract.userRecordByUID(farmerUID);
+      expect(farmerRecord.account).to.equal(await farmer.getAddress());
+      expect(farmerRecord.name).to.equal("Bob Farmer");
     });
   });
 });
